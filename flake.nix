f00f4878c242d3507805c0d9a625dee50223e7b2
aemogie <54412618+aemogie@users.noreply.github.com>
2024-04-12 10:32:07 +0530

    nix: rust-analyzer in devshell

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs;
           mkShell {
-            buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy];
+            buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
           };
       }
